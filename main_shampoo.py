a609e13b3409cc194d706dc7f5d57a13e6b38fe9
Mahyar JafariNodeh <136162130+mahyarjn80@users.noreply.github.com>
2025-10-06 22:58:02 -0400

    added muon support

--- main_shampoo.py
+++ main_shampoo.py
@@ -333,6 +333,63 @@ class Adam(torch.optim.Optimizer):
         return loss
 
 
+@torch.compile
+def zeropower_via_newtonschulz5(G, steps=3, eps=1e-7):
+    """
+    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+    zero even beyond the point where the iteration no longer converges all the way to one everywhere
+    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+    where S' is diagonal with S_{ii}' \sim Uniform(0.5, 1.5), which turns out not to hurt model
+    performance at all relative to UV^T, where USV^T = G is the SVD.
+    """
+    assert len(G.shape) == 2
+    a, b, c = (3.4445, -4.7750,  2.0315)
+    X = G.bfloat16()
+    X /= (X.norm() + eps) # ensure top singular value <= 1
+    if G.size(0) > G.size(1):
+        X = X.T
+    for _ in range(steps):
+        A = X @ X.T
+        B = b * A + c * A @ A
+        X = a * X + B @ X
+    if G.size(0) > G.size(1):
+        X = X.T
+    return X
+
+
+class Muon(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-3, momentum=0, nesterov=False):
+        if lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        if momentum < 0.0:
+            raise ValueError(f"Invalid momentum value: {momentum}")
+        if nesterov and momentum <= 0:
+            raise ValueError("Nesterov momentum requires a momentum")
+        defaults = dict(lr=lr, momentum=momentum, nesterov=nesterov)
+        super().__init__(params, defaults)
+
+    def step(self):
+        for group in self.param_groups:
+            lr = group['lr']
+            momentum = group['momentum']
+            for p in group['params']:
+                g = p.grad
+                if g is None:
+                    continue
+                state = self.state[p]
+
+                if 'momentum_buffer' not in state.keys():
+                    state['momentum_buffer'] = torch.zeros_like(g)
+                buf = state['momentum_buffer']
+                buf.mul_(momentum).add_(g)
+                g = g.add(buf, alpha=momentum) if group['nesterov'] else buf
+
+                p.data.mul_(len(p.data)**0.5 / p.data.norm()) # normalize the weight
+                update = zeropower_via_newtonschulz5(g.reshape(len(g), -1)).view(g.shape) # whiten the update
+                p.data.add_(update, alpha=-lr) # take a step
+
 
 #############################################
 #                DataLoader                 #
@@ -503,7 +560,8 @@ ValidArch = Union[CNN, MLP, VIT, LSTM, Mamba, Transformer, Resnet]
 
 
 def main(
-    arch: ValidArch,                  # which architecture to train
+    arch: ValidArch,        
+    opt: str="shampoo",          # which architecture to train
     data_path: str = "cifar10",       # path to store CIFAR10 data
     batch_size: int = 2000,           # batch size for trainin               
     lr_bias: float = 0.01,            # learning rate for biases
@@ -567,7 +625,12 @@ def main(
     optimizer1 = Adam(param_configs_sgd, lr=lr_bias, betas=(0.9, 0.95), eps=1e-10, weight_decay=weight_decay) if param_configs_sgd else None
     
     # Optimizer 2: Shampoo for filter parameters
+    if opt == "shampoo":
     optimizer2 = Shampoo(filter_params, lr=lr_filters, momentum=momentum_shampoo, weight_decay=weight_decay) if len(filter_params) > 0 else None
+    elif opt == "muon":
+        optimizer2 = Muon(filter_params, lr=0.24, momentum=0.6, nesterov=True) if len(filter_params) > 0 else None
+    else:
+        raise ValueError(f"Invalid optimizer: {opt}")
     
     optimizers = [opt for opt in [optimizer1, optimizer2] if opt is not None]
     
